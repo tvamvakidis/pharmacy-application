84c2627f32c337bcbf9ebcb2328827e028a1354e
Theodoros <theo161@gmail.com>
2025-09-27 22:39:54 +0300

    -create category service

---
+++ src/main/java/gr/vamvakidis/pharmacy_application/service/CategoryService.java
@@ -0,0 +1,80 @@
+package gr.vamvakidis.pharmacy_application.service;
+
+import gr.vamvakidis.pharmacy_application.entity.Category;
+import gr.vamvakidis.pharmacy_application.exception.ResourceNotFoundException;
+import gr.vamvakidis.pharmacy_application.repository.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+
+    /**
+     * Retrieves all categories from the database.
+     *
+     * @return List of all Category entities
+     */
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+
+    /**
+     * Retrieves a single category by its ID.
+     * Throws ResourceNotFoundException if the category does not exist.
+     *
+     * @param id The ID of the category
+     * @return The Category entity with the given ID
+     */
+    public Category getCategoryById(Long id) {
+        return categoryRepository.findById(id).
+                orElseThrow(() -> new ResourceNotFoundException("Category", id));
+    }
+
+
+    /**
+     * Creates a new category in the database.
+     *
+     * @param category The Category entity to create
+     * @return The saved Category entity
+     */
+    public Category createCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+
+    /**
+     * Updates an existing category by its ID.
+     * Throws ResourceNotFoundException if the category does not exist.
+     *
+     * @param id The ID of the category to update
+     * @param category The details to update
+     * @return The updated Category entity
+     */
+    public Category updateCategory(Long id, Category category) {
+        Category existingCategory = getCategoryById(id);
+        existingCategory.setName(category.getName());
+        existingCategory.setDescription(category.getDescription());
+        return categoryRepository.save(existingCategory);
+    }
+
+
+    /**
+     * Deletes an existing category by its ID.
+     * Throws ResourceNotFoundException if the category does not exist.
+     *
+     * @param id The ID of the category to delete
+     */
+    public void deleteCategory(Long id) {
+        Category existingCategory = getCategoryById(id);
+        categoryRepository.delete(existingCategory);
+    }
+}
