fc416d2d8708e6307133b2a0d78357531b18b40b
Theodoros <theo161@gmail.com>
2025-09-27 22:29:52 +0300

    -create 3 repos -custom query on the transaction repo to answer question 3 of the assignment

---
+++ src/main/java/gr/vamvakidis/pharmacy_application/repository/MedicineRepository.java
@@ -0,0 +1,9 @@
+package gr.vamvakidis.pharmacy_application.repository;
+
+import gr.vamvakidis.pharmacy_application.entity.Medicine;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MedicineRepository extends JpaRepository<Medicine,Long> {
+}
---
+++ src/main/java/gr/vamvakidis/pharmacy_application/repository/TransactionRepository.java
@@ -0,0 +1,29 @@
+package gr.vamvakidis.pharmacy_application.repository;
+
+import gr.vamvakidis.pharmacy_application.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface TransactionRepository extends JpaRepository<Transaction,Long> {
+
+    /**
+     * Retrieves all "OUT" type transactions for a specific medicine
+     * within a given date range.
+     *
+     * @param medicineId   The ID of the medicine to filter by
+     * @param startingDate The start of the date range (inclusive)
+     * @param endingDate   The end of the date range (inclusive)
+     * @return A list of Transaction entities that match the criteria
+     */
+    @Query("SELECT t FROM Transaction t " +
+            "WHERE t.medicine.id = :medicineId " +
+            "AND t.type = 'OUT' " +
+            "AND t.date BETWEEN :startingDate AND :endingDate")
+    List<Transaction> getTransactionsOut(@Param("medicineId") Long medicineId,
+                                         @Param("startingDate")LocalDateTime startingDate,
+                                         @Param("endingDate") LocalDateTime endingDate);
+}
