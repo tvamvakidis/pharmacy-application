c102401ffd556dc0813855fad78d161edc3bd9fe
Theodoros Vamvakidis <tvamvakidis@ots.gr>
2025-12-11 12:12:29 +0200

    -add @Repository annotation to TransactionRepository

--- src/main/java/gr/vamvakidis/pharmacy_application/repository/TransactionRepository.java
+++ src/main/java/gr/vamvakidis/pharmacy_application/repository/TransactionRepository.java
@@ -4,10 +4,12 @@ import gr.vamvakidis.pharmacy_application.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
     /**
